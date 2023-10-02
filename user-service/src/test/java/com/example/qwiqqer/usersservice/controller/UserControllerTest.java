87dfe4ea6c4a166eb64f67c7a5176b48e151df87
Сергей Пацеев <serega.patseev@gmail.com>
2023-10-02 20:31:51 +0300

    create test for user controller

--- user-service/src/test/java/com/example/qwiqqer/usersservice/controller/UserControllerTest.java
+++ user-service/src/test/java/com/example/qwiqqer/usersservice/controller/UserControllerTest.java
@@ -5,9 +5,7 @@ import com.example.qwiqqer.usersservice.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -18,7 +16,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.UUID;
 
 @WebMvcTest(controllers = UserController.class)
 class UserControllerTest {
@@ -71,5 +70,43 @@ class UserControllerTest {
 						Matchers.is(HttpStatus.BAD_REQUEST.value())));
 
 		Mockito.verify(userService, Mockito.never()).saveUser(userRequest);
+	}
+
+	@Test
+	public void shouldActivateUser() throws Exception {
+		String personalCode = UUID.randomUUID().toString();
+
+		MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get(String.format("/api/v1/users?access_code=%s", personalCode));
+
+		Mockito.when(userService.activateUser(personalCode)).thenReturn(true);
+
+		mockMvc.perform(requestBuilder)
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.status",
+						Matchers.is(HttpStatus.OK.value())))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.message",
+						Matchers.is("User successfully activated.")));
+
+		Mockito.verify(userService, Mockito.times(1)).activateUser(personalCode);
+	}
+
+	@Test
+	public void shouldNotActivateUser() throws Exception {
+		String personalCode = UUID.randomUUID().toString();
+		MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get(String.format("/api/v1/users?access_code=%s", personalCode));
+
+		Mockito.when(userService.activateUser(personalCode)).thenReturn(false);
+
+		mockMvc.perform(requestBuilder)
+				.andExpect(MockMvcResultMatchers.status().isBadRequest())
+				.andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.notNullValue()))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.status",
+						Matchers.is(HttpStatus.BAD_REQUEST.value())))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.message",
+						Matchers.is("Incorrect data.")));
+
+		Mockito.verify(userService, Mockito.times(1)).activateUser(personalCode);
 	}
 }
